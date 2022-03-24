b20d31403d7ec6a163834140049baad3510243fc
Yvan <i@zhuyunfeng.com>
2022-03-24 23:51:40 +0800

    :sparkles: page version

--- js/catalog.js
+++
@@ -1,3 +0,0 @@
-console.log('this is catalog page', location.href.split('/')[4]);
-
-document.cookie = 'courseId:  location.href.split('/')[4]'
--- js/home.js
+++
@@ -1,5 +0,0 @@
-console.log('this is home page');
-document.addEventListener('hashchange', ()=>{
-  console.log('hashchange', location.href);
-})
-let id = 1;
